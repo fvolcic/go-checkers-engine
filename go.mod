4ceda555efa669895107ff2326f80dd97a82c32f
Franklin Volcic <franklinvolcic@Franklins-Laptop.local>
2024-09-01 15:42:32 -0600

    initial commit for checkers engine in Go

---
+++ go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.22.5
+
+require github.com/fvolcic/gocheckers v0.1.1-alpha
---
+++ main.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	"fmt"
+
+	gocheckers "github.com/fvolcic/gocheckers"
+)
+
+const (
+	depth = 8
+)
+
+var searchedCount int
+
+func findBestMove(board *gocheckers.CheckersBoard, blackPlayer bool) []int {
+
+	moves := board.GenerateMoves()
+
+	if len(moves) == 1 {
+		return moves[0]
+	}
+
+	searchedCount = 0
+
+	bestMove := moves[0]
+	bestValue := minimax(board, depth, blackPlayer)
+
+	for i := 1; i < len(moves); i++ {
+		boardCopy := board.GenerateDeepCopy()
+		boardCopy.MakeMove(moves[i])
+
+		value := minimax(boardCopy, depth, blackPlayer)
+
+		if blackPlayer {
+			if value > bestValue {
+				bestValue = value
+				bestMove = moves[i]
+			}
+		} else {
+			if value < bestValue {
+				bestValue = value
+				bestMove = moves[i]
+			}
+		}
+	}
+	fmt.Printf("Searched %d positions\n", searchedCount)
+	return bestMove
+}
+
+func evaluate(board *gocheckers.CheckersBoard) int {
+	raw := board.GetUnpaddedBoard()
+
+	eval := 0
+
+	blackPieceCount := 0
+	blackKingCount := 0
+
+	whitePieceCount := 0
+	whiteKingCount := 0
+
+	for row := 0; row < len(raw); row++ {
+		for col := 0; col < len(raw[row]); col++ {
+			switch raw[row][col] {
+			case 1:
+				blackPieceCount++
+			case 2:
+				whitePieceCount++
+			case 3:
+				blackKingCount++
+				blackPieceCount++
+			case 4:
+				whiteKingCount++
+				whitePieceCount++
+			}
+		}
+	}
+
+	eval += 100*(blackPieceCount-whitePieceCount) + 200*(blackKingCount-whiteKingCount)
+
+	return eval
+}
+
+func minimax(board *gocheckers.CheckersBoard, depth int, maximizingPlayer bool) int {
+
+	searchedCount++
+
+	if depth == 0 {
+		return evaluate(board)
+	}
+
+	if maximizingPlayer {
+		bestValue := -1000000
+		moves := board.GenerateMoves()
+
+		for i := 0; i < len(moves); i++ {
+			board.MakeMove(moves[i])
+			boardCopy := board.GenerateDeepCopy()
+			value := minimax(boardCopy, depth-1, false)
+			bestValue = max(bestValue, value)
+		}
+
+		return bestValue
+	} else {
+		bestValue := 1000000
+		moves := board.GenerateMoves()
+
+		for i := 0; i < len(moves); i++ {
+			board.MakeMove(moves[i])
+			boardCopy := board.GenerateDeepCopy()
+			value := minimax(boardCopy, depth-1, true)
+			bestValue = min(bestValue, value)
+		}
+
+		return bestValue
+	}
+}
+
+func main() {
+
+	board := gocheckers.NewCheckersBoard()
+	for {
+
+		fmt.Println(board.ToString())
+
+		if board.GetTurn() == 1 {
+			fmt.Println("Black's turn")
+
+			// fmt.Println("black is thinking...")
+
+			// move := findBestMove(board, true)
+			// board.MakeMove(move)
+			// continue
+		} else {
+
+			fmt.Println("White is thinking...")
+
+			move := findBestMove(board, false)
+			board.MakeMove(move)
+			continue
+		}
+
+		moves := board.GenerateMoves()
+
+		for i := 0; i < len(moves); i++ {
+			fmt.Printf("%d.) ", i)
+			fmt.Println(moves[i])
+		}
+
+		fmt.Println(board.ToString())
+
+		fmt.Println("   32     31    30     29")
+		fmt.Println("28     27    26    25")
+		fmt.Println("   24     23    22     21")
+		fmt.Println("20     19    18    17")
+		fmt.Println("   16     15    14     13")
+		fmt.Println("12     11    10     9")
+		fmt.Println("    8      7     6      5")
+		fmt.Println(" 4      3     2     1")
+
+		fmt.Println("\nEnter your move: ")
+
+		var move int
+
+		fmt.Scan(&move)
+
+		success := board.MakeMove(moves[move])
+
+		fmt.Println("Success: ", success)
+
+	}
+
+}
